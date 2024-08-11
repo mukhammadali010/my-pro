de6561846a23b183fee2a10d7b060832499475dc
Muhammadali <test@Tests-MacBook-Pro.local>
2024-08-11 16:15:19 +0500

    sdg

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
      <Navbar/>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt suscipit harum ex obcaecati corrupti? Distinctio aliquam eaque aperiam minima consequatur ullam quos cum maxime nostrum, accusamus accusantium, aliquid architecto earum.</p>
     </>
   )
 }
--- src/Navbar/index.tsx
+++ src/Navbar/index.tsx
@@ -3,6 +3,7 @@ const Navbar = () => {
     return (
         <>
             <h1>Navbar</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum animi deleniti dolor, non ut tempore pariatur! Et mollitia, molestias illo facilis optio distinctio cum sapiente error maxime obcaecati, quis accusamus!</p>
         </>
     );
 };
